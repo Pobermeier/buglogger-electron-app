7d14d1d5b6fa2821d9d2a803b503cb2abf0455a2
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-06-24 18:16:25 +0200

    Added dynamic alerts to give feedback to user

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,48 @@
 import React, { useState } from 'react';
-import { Container, Table } from 'react-bootstrap';
+import { Container, Table, Alert } from 'react-bootstrap';
 import LogItem from './LogItem';
 import AddLogItem from './AddLogItem';
 
 const App = () => {
   const [logs, setLogs] = useState([]);
+  const [alert, setAlert] = useState({
+    show: false,
+    message: '',
+    variant: 'success',
+  });
 
   const addItem = (item) => {
+    if (item.text === '' || item.user === '' || item.priority === '') {
+      showAlert('Please enter all fields', 'danger');
+      return false;
+    }
+
     item._id = Math.floor(Math.random() * 9000) + 10000;
     item.created = new Date().toString();
     setLogs([...logs, item]);
+    showAlert('Log Added');
+  };
+
+  const showAlert = (message, variant = 'success', seconds = 3000) => {
+    setAlert({
+      show: true,
+      message,
+      variant,
+    });
+
+    setTimeout(() => {
+      setAlert({
+        show: false,
+        message: '',
+        variant: 'success',
+      });
+    }, seconds);
   };
 
   return (
     <Container>
       <AddLogItem addItem={addItem} />
+      {alert.show && <Alert variant={alert.variant}>{alert.message}</Alert>}
       <Table>
         <thead>
           <tr>
@@ -27,7 +55,7 @@ const App = () => {
         </thead>
         <tbody>
           {logs.map((log) => (
-            <LogItem key={log._id} log={log} />
+            <LogItem key={log._id} log={log} deleteItem={deleteITem} />
           ))}
         </tbody>
       </Table>
